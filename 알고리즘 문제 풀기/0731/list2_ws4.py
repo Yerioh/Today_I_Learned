8641cc702df4b49d5dda827f566dd5211443f7cf
kmJIwon <rkl45687@gmail.com>
2024-08-01 09:36:39 +0900

    어제 중단된 문제 해결 0801

--- 알고리즘 문제 풀기/0731/list2_ws4.py
+++ 알고리즘 문제 풀기/0731/list2_ws4.py
@@ -3,13 +3,25 @@ import sys
 sys.stdin = open('input_ws4.txt', 'r')
 
 T = int(input())  # 테스트 케이스의 수
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     # 배열의 크기 N, 파리채의 크기 M
     N, M = map(int, input().split())
     # 파리 배열
     flies = [list(map(int, input().split())) for _ in range(N)]
-    for i in range(N-M+1):
-        for j in range(N-M+1):
+    # 파리를 찾기 위한 델타 배열
+    delta_list = []
+    for i in range(M):
+        for j in range(M):
+            delta_list.append([i, j])
+    # 파리 찾기
+    max_fly = 0
+    for i in range(N - M + 1):
+        for j in range(N - M + 1):
             fly_sum = 0
-            for k in range(M):
-                fly_sum = flies[i][j] + flies[i][j+1] + flies[i][]
+            for d in delta_list:
+                ni = i + d[0]
+                nj = j + d[1]
+                fly_sum += flies[ni][nj]
+            if fly_sum > max_fly:
+                max_fly = fly_sum
+    print(f'#{tc} {max_fly}')

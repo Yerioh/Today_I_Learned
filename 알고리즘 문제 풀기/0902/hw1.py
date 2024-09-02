b4100c022399a8e78defd6fdac807d611bf1c44b
KimJiwon <rkl45687@gmail.com>
2024-09-02 20:58:04 +0900

    algo 0902

--- 알고리즘 문제 풀기/0902/hw1.py
+++ 알고리즘 문제 풀기/0902/hw1.py
@@ -5,22 +5,23 @@ sys.stdin = open("txt/in_hw1.txt", "r")
 
 def exchange(v, idx):  # 현재 교환 횟수, 현재 자리
     global answer
-    num = int("".join(num_board))
-    if num <= answer or idx >= n:
-        return
     if v == N:  # 교환 횟수를 채웠다면
+        num = int("".join(num_board))
         if num > answer:
             answer = num
         return
-    for i in range(n):
-        if idx == i:  # 현재 자리와 교환할 자리가 같으면 교환하지 않는 것
-            unused[i] = 1
-            exchange(v, idx + 1)
-            unused[i] = 0
-        elif not unused[i]:  # 다르면서 해당 위치가 교환이 가능하다면
-            num_board[idx], num_board[i] = num_board[i], num_board[idx]
-            exchange(v + 1, idx + 1)
-            num_board[idx], num_board[i] = num_board[i], num_board[idx]
+    if idx == n:  # 자리가 범위를 넘어간다면 뒤 두 자리만 반복하여 교환
+        num_board[n - 1], num_board[n - 2] = num_board[n - 2], num_board[n - 1]
+        exchange(v + 1, idx)
+        num_board[n - 1], num_board[n - 2] = num_board[n - 2], num_board[n - 1]
+    else:
+        for i in range(n):
+            if idx == i:  # 현재 자리와 교환할 자리가 같으면 교환하지 않는 것
+                exchange(v, idx + 1)
+            else:  # 다르면서 해당 위치가 교환이 가능하다면
+                num_board[idx], num_board[i] = num_board[i], num_board[idx]
+                exchange(v + 1, idx + 1)
+                num_board[idx], num_board[i] = num_board[i], num_board[idx]
 
 
 T = int(input())  # 테스트 케이스의 수
